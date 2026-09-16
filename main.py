05babd08d37e1909002109685552195a047497a5
thebest27011997-cmd <thebest27011997@gmail.com>
2026-09-16 16:55:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import flet as ft
 from screens import AppScreens
 
 def main(page: ft.Page):
-    # Жестко прописываем мобильные параметры страницы
     page.title = "Тест_ОВ"
     page.theme_mode = ft.ThemeMode.LIGHT
-    
-    # Указываем правильный системный путь к ассетам внутри APK
     page.assets_dir = "assets" 
     
-    # Запускаем экраны приложения
     screens = AppScreens(page)
     screens.render_login_screen()
 
-# ДЛЯ ИСКЛЮЧЕНИЯ БУДУЩИХ ОШИБОК: Передаем управление напрямую мобильному контейнеру
+# Универсальная точка входа для мобильного контейнера
 app = main
+
+if __name__ == "__main__":
+    ft.app(target=main)
