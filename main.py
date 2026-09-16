724b75a5fe60a92d949c2c923198ac681c2ebdf0
thebest27011997-cmd <thebest27011997@gmail.com>
2026-09-16 17:34:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,14 @@ import flet as ft
 from screens import AppScreens
 
 def main(page: ft.Page):
-    # Установка базовых мобильных параметров страницы
+    # Конфигурация мобильного окна
     page.title = "Тест_ОВ"
     page.theme_mode = ft.ThemeMode.LIGHT
-    
-    # Указываем путь к папке с зашифрованными вопросами и шрифтами
     page.assets_dir = "assets" 
     
-    # Инициализируем и отрисовываем стартовый экран авторизации сотрудников
+    # Инициализация интерфейса
     screens = AppScreens(page)
     screens.render_login_screen()
 
-# ДЛЯ ИСКЛЮЧЕНИЯ ATTRIBUTEERROR: Новейший стандарт инициализации мобильного контейнера serious_python
+# Единственная точка входа, которую безошибочно подхватывает контейнер смартфона
 app = main
-
-if __name__ == "__main__":
-    # Официальный запуск для Flet 1.0.0, работающий без вылетов
-    ft.run_app(main)
